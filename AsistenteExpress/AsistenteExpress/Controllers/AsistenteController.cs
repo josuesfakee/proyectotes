2a8ef1d8860271884d75fda9a62555ec7157a058
Javier Vargas Ruiz <javiervargasruiz94@gmail.com>
2021-09-25 22:04:44 -0500

    AddPartial

--- AsistenteExpress/AsistenteExpress/Controllers/AsistenteController.cs
+++ AsistenteExpress/AsistenteExpress/Controllers/AsistenteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsistenteExpress.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,49 @@ namespace AsistenteExpress.Controllers
     //Just test
     public class AsistenteController : Controller
     {
+        private EdenredContext _con = new EdenredContext();
         // GET: Asistente
         public ActionResult Index()
         {
-            return View();
+            Proceso proceso = new Proceso();
+            proceso.Campañas = _con.Campanias.ToList().ConvertAll(Campaña =>
+            {
+                return new SelectListItem()
+                {
+                    Text = Campaña.Descripcion.ToString(),
+                    Value = Campaña.Id.ToString(),
+                    Selected = false
+                };
+            });
+            proceso.Perfiles = _con.Perfiles.ToList().ConvertAll(Campaña =>
+            {
+                return new SelectListItem()
+                {
+                    Text = Campaña.Descripcion.ToString(),
+                    Value = Campaña.Id.ToString(),
+                    Selected = false
+                };
+            });
+            proceso.Motivos = _con.Motivos.ToList().ConvertAll(Campaña =>
+            {
+                return new SelectListItem()
+                {
+                    Text = Campaña.Descripcion.ToString(),
+                    Value = Campaña.Id.ToString(),
+                    Selected = false
+                };
+            });
+            proceso.SubMotivos = _con.Submotivos.ToList().ConvertAll(Campaña =>
+            {
+                return new SelectListItem()
+                {
+                    Text = Campaña.Descripcion.ToString(),
+                    Value = Campaña.Id.ToString(),
+                    Selected = false
+                };
+            });
+
+            return View(proceso);
         }
 
         // GET: Asistente/Details/5
---
+++ AsistenteExpress/AsistenteExpress/Models/Proceso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsistenteExpress.Models
+{
+    public class Proceso
+    {
+        public List<SelectListItem> Campañas { get; set; }
+        public List<SelectListItem> Perfiles { get; set; }
+        public List<SelectListItem> Motivos { get; set; }
+        public List<SelectListItem> SubMotivos { get; set; }
+
+
+    }
+}
