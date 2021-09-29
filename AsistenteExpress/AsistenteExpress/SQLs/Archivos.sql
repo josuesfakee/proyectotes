2f061b4c58d3253f8d03408a06dcc9602eedbf31
Javier Vargas Ruiz <javiervargasruiz94@gmail.com>
2021-09-28 22:48:47 -0500

    confirm final

--- AsistenteExpress/AsistenteExpress/SQLs/Archivos.sql
+++ AsistenteExpress/AsistenteExpress/SQLs/Archivos.sql
@@ -1,7 +1,7 @@
 USE [AsistentExpress]
 GO
 
-/****** Object:  Table [dbo].[Archivos]    Script Date: 23/09/2021 11:00:32 p. m. ******/
+/****** Object:  Table [dbo].[Archivos]    Script Date: 28/09/2021 10:47:41 p. m. ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,13 +10,14 @@ GO
 
 CREATE TABLE [dbo].[Archivos](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdProceso] [int] NOT NULL,
 	[Usuario] [nvarchar](250) NULL,
 	[NameFile] [nvarchar](250) NULL,
 	[ruta] [nvarchar](250) NULL,
 	[typeFile] [nvarchar](200) NULL,
 	[FechaCarga] [datetime] NULL,
 	[Estatus] [bit] NOT NULL,
-PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Archivos] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
